425a0e8373d3caaed01269b8fcca551fd0f295dd
chikakow <chikakow@gmail.com>
2021-08-03 17:20:29 -0400

    till 5 03

---
+++ src/Query.js
@@ -0,0 +1,29 @@
+const githubQuery = (pageCount, queryString) => {
+    return {
+      query: `
+      {
+        viewer {
+          name
+        }
+        search(query: "${queryString}user:planetoftheweb sort:updated-desc", type: REPOSITORY, first: ${pageCount}) {
+          repositoryCount
+          nodes {
+            ... on Repository {
+              name
+              description
+              id
+              url
+              viewerSubscription
+              licenseInfo {
+                spdxId
+              }
+            }
+          }
+        }
+      }
+    `,
+    };
+  };
+  
+  export default githubQuery;
+  
